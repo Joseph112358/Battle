5588468eddae4d85c10c0712cfe29b52f3a87c77
Joe <josephshaw1563@gmail.com>
2021-10-06 16:57:32 +0100

    takes in names

---
+++ app.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+# get '/' do 
+#   @player1 = params[:player1]
+#   @player2 = params[:player2]
+#   erb(:home)
+# end
+class Battle < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    erb :home
+  end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
